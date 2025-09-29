b275caacbbfffe457f1e841fe74091fe0db893ba
htuan2800 <huynhngoctuan48@gmail.com>
2025-09-29 15:56:56 +0700

    test
    test

---
+++ src/test/java/com/volunteerBackend/VolunteerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
